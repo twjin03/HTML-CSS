148682e34ae60b9837e9d46672cd2117ad86ab76
twjin03 <twjin03@g.skku.edu>
2025-03-12 06:21:17 +0900

    feat: hide keyboard before start

--- Wordle/index.js
+++ Wordle/index.js
@@ -230,6 +230,8 @@ function startGame() {
     drawGrid();
     resetKeyboard();
 
+    document.querySelector('.keyboard').classList.add('visible');
+
     const startButton = document.getElementById("start-btn");
     startButton.textContent = "Restart Game";
     startButton.style.backgroundColor = "#b59f3b";
